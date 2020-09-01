3c32ff19eec75d11e3d7795e746a7d999b737d5a
Shawn Edwards <shedwards@nvidia.com>
2020-08-31 19:47:21 -0500

    Triple the number of retries and increase the delay between them

--- s3-artifact-storage-common/src/main/java/jetbrains/buildServer/artifacts/s3/S3Constants.java
+++ s3-artifact-storage-common/src/main/java/jetbrains/buildServer/artifacts/s3/S3Constants.java
@@ -42,9 +42,9 @@ public class S3Constants {
   public static final String S3CONFIG_MINIMUM_UPLOAD_PART_SIZE = "storage.s3.upload.config.minimumUploadPartSize";
   public static final String S3CONFIG_MULTIPART_UPLOAD_THRESHOLD = "storage.s3.upload.config.multipartUploadThreshold";
 
-  public static final int DEFAULT_S3_URL_LIFETIME_SEC = 60;
-  public static final int DEFAULT_S3_RETRY_DELAY_ON_ERROR_MS = 1000;
-  public static final int DEFAULT_S3_NUMBER_OF_RETRIES_ON_ERROR = 5;
+  public static final int DEFAULT_S3_URL_LIFETIME_SEC = 3600;
+  public static final int DEFAULT_S3_RETRY_DELAY_ON_ERROR_MS = 5000;
+  public static final int DEFAULT_S3_NUMBER_OF_RETRIES_ON_ERROR = 15;
 
   public static final String ARTEFACTS_S3_UPLOAD_PRESIGN_URLS_HTML = "/artefacts/s3/upload/presign-urls.html";
 }
